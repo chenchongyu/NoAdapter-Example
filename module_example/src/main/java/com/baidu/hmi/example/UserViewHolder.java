071866cd24fd237e64528557206a4c0a6c3c8a59
chenchongyu <chenchongyu@baidu.com>
2021-03-15 11:22:33 +0800

    code style

--- module_example/src/main/java/com/baidu/hmi/example/UserViewHolder.java
+++ module_example/src/main/java/com/baidu/hmi/example/UserViewHolder.java
@@ -8,11 +8,13 @@ import com.baidu.hmi.adpater.BaseVH;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+
 import com.baidu.hmi.annotation.ViewHolder;
 
 @ViewHolder
 public class UserViewHolder extends BaseVH<Student> {
     TextView textView;
+
     public UserViewHolder(@NonNull ViewGroup parent) {
         super(parent, R.layout.item_user);
         textView = itemView.findViewById(R.id.v_name);
